9dc3b614f5576f493f7e5e9a020fd0ca797f94a0
salomonsky342341 <129327712+salomonsky342341@users.noreply.github.com>
2023-03-31 21:46:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,105 +1,77 @@
 import streamlit as st
 import requests
-import json
-import imageio
-import urllib
-import os
-from transformers import GPTNeoForCausalLM, GPT2Tokenizer
-from transformers import pipeline, set_seed
-from PIL import Image
+from io import BytesIO
+from deepdreamgenerator import deepdreamgenerator
+from moviepy.editor import *
 
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+os.environ['IMAGEIO_FFMPEG_EXE'] = os.path.join("path", "to", "ffmpeg.exe")
 
-# Configuración de la API de Unsplash
-UNSPLASH_ACCESS_KEY = os.environ.get("QKOHMYhWLib3jioqFghx2ZUJXpiH_f1K5aHt_1okJbI")
-api_url = "https://api.unsplash.com/photos/random?orientation=landscape&per_page=1"
+WPM = 200 # palabras por minuto
+CLIP_DURATION = 5 # duración en segundos de cada clip de video
 
-# Configuración de la API de ImageBB
-IMGBB_API_KEY = os.environ.get("ec3a2919230df7600e65ff9e0e5523db")
-IMGBB_UPLOAD_URL = "https://api.imgbb.com/1/upload"
+def generate_story(length):
+    response = requests.get(f"https://api.chatterscan.com/gpt3?model=curie-instruct-beta-001&length={length}")
+    return response.json()["text"].split("\n\n")
 
-# Configuración del modelo de generación de texto
-model = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B', device=0)
+def generate_voiceover(paragraph):
+    # Generar una voz en off a partir del párrafo usando una API de síntesis de voz
+    # (por ejemplo, Google Text-to-Speech o Amazon Polly)
+    voiceover = ... # código para generar la voz en off
+    return voiceover
 
+def generate_video(story):
+    clips = []
+    for i, paragraph in enumerate(story):
+        # Generar la imagen de sueño profundo correspondiente al párrafo
+        imagebytes = deepdreamgenerator(paragraph)
+        imageclip = ImageClip(BytesIO(imagebytes)).resize(height=720)
 
-def generate_story(idea):
-    set_seed(42)
-    story = model(idea, max_length=100, do_sample=True)
-    return [sent.capitalize() for sent in story[0]['generated_text'].split('.')][:5]
+        # Generar la voz en off correspondiente al párrafo y calcular la duración del clip
+        voiceover = generate_voiceover(paragraph)
+        num_words = len(paragraph.split())
+        clip_duration = num_words / WPM
 
+        # Crear el clip de video compuesto por la imagen y la voz en off
+        audio = AudioFileClip(voiceover).set_duration(clip_duration)
+        audio = audio.set_start(0) # empezar al principio del clip
+        imageclip = imageclip.set_duration(clip_duration)
+        imageclip = imageclip.set_start(0) # empezar al principio del clip
+        compositeclip = CompositeVideoClip([imageclip, audio.set_pos(('center', 'bottom'))])
 
-def get_image():
-    headers = {"Authorization": f"Client-ID {UNSPLASH_ACCESS_KEY}"}
-    response = requests.get(api_url, headers=headers)
-    json_data = json.loads(response.text)
-    image_url = json_data[0]["urls"]["regular"]
-    return image_url
+        clips.append(compositeclip)
 
+    # Concatenar los clips de video para crear el video completo
+    videoclip = concatenate_videoclips(clips)
 
-def generate_gif(image_urls):
-    images = []
-    for url in image_urls:
-        image = Image.open(urllib.request.urlopen(url))
-        image = image.resize((512, 512))
-        images.append(image)
-    imageio.mimsave("story.gif", images, duration=1)
+    # Guardar el video en un archivo
+    videofilename = "story.mp4"
+    videoclip.write_videofile(videofilename, fps=24)
 
-
-def upload_to_imgbb(filename):
-    with open(filename, "rb") as file:
-        url = IMGBB_UPLOAD_URL
-        payload = {"key": IMGBB_API_KEY, "image": file.read()}
-        res = requests.post(url, payload)
-        if res.ok:
-            return res.json()["data"]["url"]
-        else:
-            return None
+    return videofilename
 
 
-# Configuración de la interfaz de usuario con Streamlit
-st.title("Generador de historias e imágenes")
-
-option = st.sidebar.selectbox(
-    "Selecciona una opción:",
-    ("Escribir una idea", "Previsualizar la historia generada por IA", "Previsualizar el GIF resultado del programa")
-)
-
-if option == "Escribir una idea":
-    idea = st.text_input("Escribe una idea para generar la historia:")
-    st.write("Presiona el botón para generar la historia.")
-    if st.button("Generar historia"):
-        try:
-            story = generate_story(idea)
-            st.write("Historia generada:")
-            for i, sent in enumerate(story):
-                st.write(f"{i+1}. {sent}")
-            st.session_state["story"] = story
-        except Exception as e:
-            st.write("Ocurrió un error al generar la historia.")
-            st.write(e)
-
-elif option == "Previsualizar la historia generada por IA":
-    if "story" in st.session_state:
-        story = st.session_state["story"]
-        st.write("Historia generada:")
-        for i, sent in enumerate(story):
-            st.write(f"{i+1}. {sent}")
-    else:
-        st.write("Primero debes generar una historia.")
-
-elif option == "Previsualizar el GIF resultado del programa":
-    if "story" in st.session_state:
-        story = st.session_state["story"]
-        st.write("Generando imágenes...")
-        image_urls = []
-
-        for i in range(5):
-            image_url = get_image()
-            image_urls.append(image_url)
-
-        generate_gif(image_urls)
-        st.write("Subiendo GIF a ImageBB...")
-        url = upload_to_imgbb("story.gif")
-        st.write(f"¡Listo! Puedes ver tu GIF en {url}")
-    else:
-        st.write("Primero debes generar una historia.")
+st.title("Generador de Historias")
+st.write("Este es un generador automático de historias. Ingresa tu idea principal y presiona el botón para generar una historia aleatoria y un video personalizado.")
+idea_principal = st.text_input("Ingresa tu idea principal")
+if st.button("Generar Historia"):
+    length = 5
+    story = generate_story(idea_principal, length)
+    st.write("Historia generada:")
+    for i, paragraph in enumerate(story):
+        st.write(f"{i+1}. {paragraph}")
+    st.write("Generando video…")
+    videofilename = generate_video(story)
+    st.write("Video generado:")
+    videofile = open(videofilename, "rb")
+    videobytes = videofile.read()
+    st.video(videobytes)
+    st.write("Previsualizando video…")
+    preview_clip = VideoFileClip(videofilename).subclip(0, 10)
+    preview_clip = preview_clip.resize(height=360)
+    preview_file = "preview.mp4"
+    preview_clip.write_videofile(preview_file, fps=24)
+    preview_file = open(preview_file, "rb")
+    preview_bytes = preview_file.read()
+    st.video(preview_bytes)
+else:
+    st.write("Ingresa tu idea principal y presiona el botón para generar una historia y video personalizados.")
